7c15d499fc540bcd247c94b4661f5f10d7e49fcd
benelser <benjamin.elser18@gmail.com>
2021-02-25 11:47:22 -0600

    changed data source file read location string

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "template_file" "sa_definition" {
-  template = file("${var.service_account_file_name}")
+  template = file(var.service_account_file_name)
   vars = {
     project_id = "${var.project_id}"
     environment = "${var.environment}"
