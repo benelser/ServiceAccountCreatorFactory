fdf9cee30ed83daf62ce0a36b8c2368ef043fd9f
benelser <benjamin.elser18@gmail.com>
2021-02-25 11:38:03 -0600

    Updated main to decode rendered data source

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
+locals {
+  sa = jsondecode(data.template_file.sa_definition.rendered)
+}
+
 resource "google_service_account" "service_account" {
-  account_id   = data.template_file.sa_definition.rendered.name
-  project      = data.template_file.sa_definition.rendered.owner_project_id
-  display_name = data.template_file.sa_definition.rendered.display_name
-  description = data.template_file.sa_definition.rendered.description
+  account_id   = local.sa.name
+  project      = local.sa.owner_project_id
+  display_name = local.sa.display_name
+  description = local.sa.description
 }
 module "sa_project_level_bind" {
-    count = length(data.template_file.sa_definition.rendered.project_level_iam_bindings)
+    count = length(local.sa.project_level_iam_bindings)
     counter = count.index
     email = google_service_account.service_account.email
     source = "./modules/sa_project_level_bind"
-    service_account = data.template_file.sa_definition.rendered
+    service_account = local.sa
 }
 module "sa_level_self_bind" {
-    count = length(data.template_file.sa_definition.rendered.sa_level_self_iam_bindings)
+    count = length(local.sa.sa_level_self_iam_bindings)
     counter = count.index
     service_account_id = google_service_account.service_account.name
     source = "./modules/sa_level_self_bind"
-    service_account = data.template_file.sa_definition.rendered
+    service_account = local.sa
 }
